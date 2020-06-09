0979eb1b9e636d4a67241bca45cfc5e6736b41a1
Guannan-Shen <guannan.shen.ai@gmail.com>
2020-06-09 12:40:46 -0600

    run_debug_vscode

--- Developer_Basics/cpp/tricks/hello_world.cpp
+++ Developer_Basics/cpp/tricks/hello_world.cpp
@@ -4,6 +4,9 @@
 
 using namespace std;
 
+// tasks.json to build multiple 
+// C++ files by using an argument like "${workspaceFolder}/*.cpp" instead of ${file}.
+
 int main()
 {
     vector<string> msg {"Hello", "C++", "world","from","VS Code", "and the C++ extension!"};
