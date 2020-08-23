c6905da59dcdbb10e81ccb517bd3fdaa080aa06a
Guannan-Shen <guannan.shen.ai@gmail.com>
2020-08-23 08:13:36 -0600

    node of linked list

---
+++ Algo/Python/linkedList.py
@@ -0,0 +1,7 @@
+# define a node class, node in linked list
+class node(object): # the object is the identifier, base object in python to inherit
+
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        
