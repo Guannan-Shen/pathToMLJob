62915b0d9774873ef08fab95bf926d09d33b14f3
Guannan-Shen <guannan.shen.ai@gmail.com>
2020-09-03 03:42:33 -0600

    stack in swift use resizable array

--- Algorithms/swift/dataStructure.swift
+++ Algorithms/swift/dataStructure.swift
@@ -17,3 +17,28 @@ var averPet = sum/numOfStu
 
 print(averPet)
 
+// TODO: Stack
+class Stack {
+    // use a resizable array
+    var stackArray = [String]()
+    // push
+    func push(item:String){
+        self.stackArray.append(item)
+    }
+
+    // pop
+    // ? optional type
+    func pop()->String?{
+        if self.stackArray.last != nil {
+            let lastString = self.stackArray.last
+            self.stackArray.removeLast()
+            // ! wrap the optional output, there exist or do not exist output
+            return lastString!
+        } else {
+            return nil
+        }
+    }
+}
+
+// instance
+var deck:Stack = Stack()
