1001da44ead992fc3a5777f90ee382ab4f2d35db
Guannan-Shen <guannan.shen.ai@gmail.com>
2020-09-09 02:09:13 -0600

    test java array, for loop

---
+++ Developer_Basics/Java/hello_world.java
@@ -0,0 +1,8 @@
+public class test{
+    public static void main(String[] args){
+        int[] studentId = new int[10];
+        for(i = 0; i < 10; i++){
+            System.out.println("Hello World for student: " + i);
+        }
+    }
+}
