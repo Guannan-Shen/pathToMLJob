f48c3de608a4f3189e849a86f9e930fbaf777158
Guannan-Shen <guannan.shen.ai@gmail.com>
2020-09-20 20:55:18 -0600

    name of public class main method, filename.java

--- Developer_Basics/Java/hello_world.java
+++ Developer_Basics/Java/hello_world.java
@@ -1,5 +1,6 @@
 // the public type must be defined in its own file
-class testHello{
+// the name of the public class should be the same with the file name
+public class hello_world{
     public static void main(String[] args){
         int[] studentId = new int[10];
         for(int i = 0; i < studentId.length; i++){
