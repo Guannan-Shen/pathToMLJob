996152f781438ac2d81da7b8ffeeb02f11e941d2
Chelsea-Cheng99 <chelsea942995612@gmail.com>
2020-10-06 04:02:32 -0600

    BufferedReader FileReader in Java

---
+++ Developer_Basics/Java/ifTest.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ifTest {
+    public static void main (String [] args){
+        System.out.println("Pick a integer between 1 and 10");
+
+        try (Scanner scanner = new Scanner(System.in))
+        
+        {
+            
+        }
+    }
+}
